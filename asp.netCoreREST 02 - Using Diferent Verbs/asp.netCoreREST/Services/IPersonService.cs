5f906cee0674fcb0a116651709a66f0b8edd4d4c
Gabriel Lemos <gabrielelias.lemos@live.com>
2019-03-19 09:39:46 -0300

    Implementing PersonService

---
+++ asp.netCoreREST 02 - Using Diferent Verbs/asp.netCoreREST/Services/IPersonService.cs
@@ -0,0 +1,12 @@
+﻿using asp.netCoreREST.Model;
+using System.Collections.Generic;
+
+namespace asp.netCoreREST.Services.Implementation {
+    interface IPersonService {
+        Person Create(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
---
+++ asp.netCoreREST 02 - Using Diferent Verbs/asp.netCoreREST/Services/Implementation/PersonServiceImplementation.cs
@@ -0,0 +1,48 @@
+﻿using asp.netCoreREST.Model;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace asp.netCoreREST.Services.Implementation {
+    public class PersonServiceImplementation : IPersonService {
+        private volatile int count;
+
+        public Person Create(Person person) {
+            return person;
+        }
+
+        public Person FindById(long id) {
+            return MockPerson(id);
+        }
+
+        public List<Person> FindAll() {
+            List<Person> persons = new List<Person>();
+            for(int i = 0; i < 8; i++) {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+            return persons;
+        }
+
+        public Person Update(Person person) {
+            return person;
+        }
+
+        public void Delete(long id) { }
+
+        private Person MockPerson(long i) {
+            return new Person {
+                Id = IncrementAndGet(),
+                FirstName = "New Name" + i,
+                LastName = "New Lastname" + i,
+                Address = "New Address" + i,
+                Gender = "New Gender"
+
+            };
+        }
+
+        private long IncrementAndGet() {
+            return Interlocked.Increment(ref count);
+        }
+    }
+}
